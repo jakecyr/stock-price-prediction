347f032cfb9f6041807d4d83cb6cc98c2bfe407a
Jake Cyr <cyrjake@gmail.com>
2020-04-10 18:47:38 -0400

    Remove node_modules

--- node_modules/brain.js/src/utilities/randos.js
+++
@@ -1,9 +0,0 @@
-const randomWeight = require('./random-weight');
-
-module.exports = function randos(size) {
-  const array = new Float32Array(size);
-  for (let i = 0; i < size; i++) {
-    array[i] = randomWeight();
-  }
-  return array;
-};
